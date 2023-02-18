652b96e264d9aaff71a63d47f1d3d96db3b198f5
andreas <podovinov2003andrey@gmail.com>
2023-02-18 17:16:44 +0600

    fix responses

--- src/models/schemas/products/products_response.py
+++ src/models/schemas/products/products_response.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class ProductsResponse(BaseModel):
     id: int
     name: str
+    created_at: datetime
+    created_by: int
+    modified_at: datetime
+    modified_by: int
 
     class Config:
         orm_mode = True
--- src/models/schemas/users/users_response.py
+++ src/models/schemas/users/users_response.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class UsersResponse(BaseModel):
     id: int
     username: str
     role: str
+    created_at: datetime
+    created_by: int = None
+    modified_at: datetime
+    modified_by: int = None
 
     class Config:
         orm_mode = True
